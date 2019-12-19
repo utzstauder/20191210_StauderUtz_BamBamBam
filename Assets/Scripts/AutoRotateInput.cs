253964b36c063a868229818c2fe204f0a9f252dc
Utz Stauder <utzstauder@users.noreply.github.com>
2019-12-19 15:18:38 +0100

    Added editor extension scripts

---
+++ Assets/Scripts/AutoRotateInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotateInput : MonoBehaviour
+{
+    public float rotationAngle = 45f;
+    public float radius = 1f;
+    public float radiusThickness = 1f;
+}
---
+++ Assets/Scripts/Editor/AutoRotateEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutoRotateInput))]
+public class AutoRotateEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        AutoRotateInput autoRotateInput = target as AutoRotateInput;
+
+        Vector3 fromVector = Quaternion.AngleAxis(
+            angle: -autoRotateInput.rotationAngle / 2f,
+            axis: autoRotateInput.transform.up
+            ) * autoRotateInput.transform.forward;
+
+        Handles.color = new Color(0, 1f, 0, 0.05f);
+        Handles.DrawSolidArc(
+            center: autoRotateInput.transform.position,
+            normal: autoRotateInput.transform.up,
+            from: fromVector,
+            angle: autoRotateInput.rotationAngle,
+            radius: autoRotateInput.radius * HandleUtility.GetHandleSize(autoRotateInput.transform.position)
+            );
+
+        Handles.color = Color.green;
+        for (float r = 0; r < autoRotateInput.radiusThickness; r += autoRotateInput.radiusThickness * 0.01f)
+        {
+            Handles.DrawWireArc(
+                center: autoRotateInput.transform.position,
+                normal: autoRotateInput.transform.up,
+                from: fromVector,
+                angle: autoRotateInput.rotationAngle,
+                radius: (autoRotateInput.radius + r) * HandleUtility.GetHandleSize(autoRotateInput.transform.position)
+                );
+        }
+
+        //Handles.BeginGUI();
+        //{
+        //    GUILayout.BeginHorizontal();
+        //    {
+        //        GUILayout.BeginVertical();
+        //        {
+        //            for (int i = 0; i < 5; i++)
+        //            {
+        //                if (GUILayout.Button("Hallo, ich bin Buton " + i))
+        //                {
+        //                    Debug.Log("Klick!");
+        //                }
+        //            }
+        //        }
+        //        GUILayout.EndVertical();
+
+        //        if (GUILayout.Button("Hallo, ich bin ein Button."))
+        //        {
+        //            Debug.Log("Klick!");
+        //        }
+        //    }
+        //    GUILayout.EndHorizontal();
+        //}
+        //Handles.EndGUI();
+    }
+}
